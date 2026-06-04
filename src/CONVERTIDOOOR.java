a0a4c4f23a6944a12fe2935847abde9d13601363
EFRAIN <efrainaguilar45632@gmail.com>
2026-06-04 14:31:29 -0600

    Initial commit

---
+++ src/CONVERTIDOOOR.java
@@ -0,0 +1,11 @@
+
+public class CONVERTIDOOOR {
+    public double millasaKilometros(double millas){
+        return millas * 1.60934;
+        
+    }
+    public double kilometrosaMillas(double kilometros){
+        return kilometros / 1.60934;
+        
+    }
+    }
